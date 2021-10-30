ab303f210cd8bb098844be9c21cadf6262c84a8b
adichandra99 <adichandraputra3@gmail.com>
2021-10-30 18:45:33 +0700

    Pra UTS WP

--- .htaccess.php
+++ .htaccess.php
@@ -1,10 +1,4 @@
-<html>
-<head>
- <title>Latihan 1</title>
-</head>
-<body>
-    Halo Kawan.. Yuk kita belajar web programming..!!!<br>
-    Hasil dari penjumlahan <?=
-    $nilai1 . " + " . $nilai2  . " yaitu  = " . $hasil; ?>
-</body>
-</html>
+RewriteEngine On
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteCond %{REQUEST_FILENAME} !-d
+RewriteRule ^(.*)$ index.php/$1 [L]
--- application/views/v_edit.php
+++ application/views/v_edit.php
@@ -1,38 +1,55 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width= , initial-scale=1.0">
-    <title>Crud dengan CodeIgniter</title>
-</head>
-<center>
-    <h1> Crud dengan CodeIgniter </h1>
-    <h3> Form Edit Data </h3>
-</center>
-<?php foreach ($user as $u) { ?>
-    <form action="<?php echo base_url(), 'crud/update'; ?>" method="post">
-        <table style="margin: 20px auto;">
-            <tr>
-                    <td>Nama</td>
-                    <td>:</td>
-                    <input type="hidden" name="id" value="<?php echo $u->id ?>">
-                    <td><input type="text" name="nama" value="<?php echo $u->nama ?>"></td>
-            </tr>
-            <tr>
-                    <td>Alamat</td>
-                    <td>:</td>
-                    <td><input type="text" name="alamat" value="<?php echo $u->alamat ?>"></td>
-            </tr>
-            <tr>
-                    <td>Pekerjaan</td>
-                    <td>:</td>
-                    <td><input type="text" name="pekerjaan" value="<?php echo $u->pekerjaan ?>"></td>
-            </tr>
-            <tr>
-                    <td><input type="submit" value="Simpan"></td>
-            </tr>
-        </table>
-    </form>
-<?php } ?>
+<html lang="en" dir="ltr">
+   <head>
+      <meta charset="utf-8">
+      <title>Crud dengan CodeIgniter</title>
+      <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/style.css">
+      <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+   </head>
+   <body>
+      <main>
+         <section class="glass">
+            <div class="dashboard3">
+               <h3>Form Edit Data</h3>
+               <div class="form">
+                  <?php foreach ($user as $u) { ?>
+                     <form action="<?php echo base_url() . 'crud/update'?>" method="post">
+                        <table style="margin:20px auto;">
+                           <tr>
+                              <td>Nama</td>
+                              <td>:</td>
+                              <input type="hidden" name="nim" value="<?php echo $u->nim ?>">
+                              <td><input type="text" name="nama" value="<?php echo $u->nama ?>"></td>
+                           </tr>
+                           <tr>
+                              <td>Kelas</td>
+                              <td>:</td>
+                              <td><input type="text" name="kelas" value="<?php echo $u->kelas ?>"></td>
+                           </tr>
+                           <tr>
+                              <td>Alamat</td>
+                              <td>:</td>
+                              <td><input type="text" name="alamat" value="<?php echo $u->alamat ?>"></td>
+                           </tr>
+                           <td><input class="submit" type="submit" value="Simpan"></td>
+                        </table>
+                     </form>
+                  <?php } ?>
+               </div>
+            </div>
+            <div class="layar">
+               <h3>CRUD dengan CodeIgniter</h3>
+            </div>
+         </section>
+      </main>
+      <div class="circle1"></div>
+      <div class="circle2"></div>
+      <center>
+         <h1>Crud dengan CodeIgniter</h1>
+         <h3>Form Edit Data</h3>
+      </center>
+   </body>
 </html>
